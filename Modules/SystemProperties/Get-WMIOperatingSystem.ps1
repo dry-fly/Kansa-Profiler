889d03027c84816b14c4042c0ecc641d1cbda0a6
Jason Simsay <jason.simsay@gmail.com>
2016-08-03 18:51:04 -0400

    Corrected comments

--- Modules/SystemProperties/Get-WMIOperatingSystem.ps1
+++ Modules/SystemProperties/Get-WMIOperatingSystem.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-Get-WMIOperatingSystem.ps1 returns information from the win32_computersystem class to aid in profiling systems based on hardware platform.
+Get-WMIOperatingSystem.ps1 returns information from the win32_operatingsystem class to aid in profiling systems based on operating system platform.
 .NOTES
 The following line is required by Kansa.ps1, which uses it to determine
 how to handle the output from this script.
